282ed41ce2431fc30eb8add3ec02dbaf02b58cbe
Marwen Arfa <marwen.arfa@gmail.com>
2022-08-29 16:14:39 +0100

    chore: setup storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { Button } from '../components/Button';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -17,12 +18,7 @@ const Home: NextPage = () => {
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
-        <button
-          type='button'
-          className='inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-        >
-          Button text
-        </button>
+        <Button label='Learn more' />
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.tsx</code>
